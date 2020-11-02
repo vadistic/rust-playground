81053951fbcc7a7433850e99314527be27c8f09c
Jakub Wadas <vadistic@gmail.com>
2020-11-02 10:42:31 +0100

    add problem1 test

---
+++ hw/src/problem2.rs
@@ -0,0 +1,9 @@
+/// Represents a matrix in row-major order
+pub type Matrix = Vec<Vec<f32>>;
+
+/// Computes the product of the inputs `mat1` and `mat2`.
+pub fn mat_mult(mat1: &Matrix, mat2: &Matrix) -> Matrix {
+  let mut res: Matrix = Matrix::new();
+
+    return res
+}
--- hw/tests/problem1.rs
+++ hw/tests/problem1.rs
@@ -1,10 +1,27 @@
+use rand::Rng;
 use hw::problem1::*;
+
 
 #[test]
 fn test_sum_small() {
     let array = [1, 2, 3, 4, 5];
 
     assert_eq!(sum(&array), 15);
+}
+
+#[test]
+fn test_sum_rand() {
+    let mut rng = rand::thread_rng();
+
+    let mut array: [i32; 32] = [0;32];
+
+    for el in &mut array {
+        *el = rng.gen_range(-1000, 1000);
+    }
+
+    let expected:i32 = array.iter().sum();
+
+    assert_eq!(sum(&array), expected);
 }
 
 // Part 2
